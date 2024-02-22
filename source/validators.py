e60983c3f62cabf1c6abf886eb2e09d49b597789
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-02-22 15:27:45 +0000

    feat: Create "validate_date()" function.

--- source/validators.py
+++ source/validators.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 def validate_integer_option(option: str, min_numb: int = 0, max_numb: int = 3) -> None:
     """Check if the option is a number between min_numb and max_numb inclusive
 
@@ -14,4 +17,18 @@ def validate_integer_option(option: str, min_numb: int = 0, max_numb: int = 3) -
 
     if not (min_numb <= option <= max_numb):
         message = f"Please enter a number between {min_numb} and {max_numb} inclusive."
+        raise ValueError(message)
+
+
+def validate_date(option: str):
+    """Check if the option is a date in format YYYY-MM-DD and if the date is not in the past
+
+    Args:
+        option (str): date string for validation
+    """
+
+    date_obj = date.fromisoformat(option)
+
+    if date_obj < date.today():
+        message = f"Your date {date_obj.isoformat()} is in the past. Please enter a future date."
         raise ValueError(message)
