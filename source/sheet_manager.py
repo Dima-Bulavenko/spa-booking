7a1302731228693ae2f3451c5ba0e404eb9f12c1
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-02-23 18:37:21 +0000

    feat: Create "get_available_times_for_date_and_service()" method.
    This is method of SpaSheet class. It calculates available ranges of time which user can book
    for paticular date and service and return them.

--- source/sheet_manager.py
+++ source/sheet_manager.py
@@ -56,3 +56,60 @@ class SpaSheet:
                 return service_data[field_name]
         
         return "Service not found"
+
+    def get_available_times_for_date_and_service(self, date_str: str, service: str) -> list[list[datetime]]:
+        """Calculates available ranges for booking and returns them.
+
+        Args:
+            date_str (str): The date to check for available time
+            service (str): The service to check for available time
+
+        Returns:
+            list[list[datetime]: List of available time
+        """
+        # Define the open and close time for the spa
+        open_time = time.fromisoformat("08:00")
+        close_time = time.fromisoformat("21:00")
+
+        # Convert the date string to date object
+        date_obj = date.fromisoformat(date_str)
+
+        # Get all bookings and the duration of the service
+        all_bookings = self.booking_data.get_all_records()
+
+        # Define timedelta object for duration of selected service
+        service_duration = timedelta(hours=self.get_service_info(service, "duration"))
+
+        service_date_bookings = []
+
+        # Loop through all bookings and get the bookings for the service and date
+        for booking in all_bookings:
+            booking_date_obj = date.fromisoformat(booking["date"])
+            if booking["spa_name"] == service and booking_date_obj == date_obj:
+                service_date_bookings.append(booking)
+        
+        # Sort the bookings by start time
+        service_date_bookings.sort(key=lambda x: time().fromisoformat(x["start_time"]))
+
+        available_times = []
+        for index in range(len(service_date_bookings) + 1):
+            if index == 0:
+                start_time = open_time
+            else:
+                start_time = time.fromisoformat(service_date_bookings[index - 1]["end_time"])
+            
+            if index == len(service_date_bookings):
+                end_time = close_time
+            else:
+                end_time = time.fromisoformat(service_date_bookings[index]["start_time"])
+            
+            # Convert to datetime object to add the service duration
+            start_time = datetime.combine(date_obj, start_time)
+            end_time = datetime.combine(date_obj, end_time)
+
+            # Create a ranges of available booking times
+            while service_duration + start_time <= end_time:
+                available_times.append([start_time, start_time + service_duration])
+                start_time += timedelta(hours=1)
+            
+        return available_times
