4aa80ca297ab56caa438e70922fe8412cf000d1b
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-03-02 01:25:17 +0000

    test: Create test for "validate_date()" function.

--- tests/test_valudators.py
+++ tests/test_valudators.py
@@ -1,6 +1,11 @@
+from datetime import date, timedelta
 from unittest import TestCase
+from unittest.mock import patch
 
-from source.validators import validate_integer_option
+# Use for mocking date.today https://stackoverflow.com/questions/4481954/trying-to-mock-datetime-date-today-but-not-working
+from freezegun import freeze_time
+
+from source.validators import validate_date, validate_integer_option
 
 
 class ValidateIntegerOption(TestCase):
@@ -37,5 +42,60 @@ class ValidateIntegerOption(TestCase):
 
         with self.assertRaises(ValueError) as context:
             validate_integer_option(value, min_numb, max_numb)
+        
+        self.assertEqual(str(context.exception), message)
+
+
+@freeze_time("1999-12-31")
+class ValidateDate(TestCase):
+    
+    def test_valid_date(self):
+        data = "1999-12-31"
+        result = validate_date(data)
+
+        self.assertIsNone(result)
+
+    def test_date_in_past(self):
+        data = "1999-12-30"
+        message = f"Your date {data} is in the past. Please enter a future date."
+
+        with self.assertRaises(ValueError) as context:
+            validate_date(data)
+        
+        self.assertEqual(str(context.exception), message)
+
+    def test_invalid_year(self):
+        data = "19999-12-31"
+        message = f"Invalid isoformat string: '{data}'"
+
+        with self.assertRaises(ValueError) as context:
+            validate_date(data)
+        
+        self.assertEqual(str(context.exception), message)
+    
+    def test_invalid_month(self):
+        data = "1999-13-31"
+        message = "month must be in 1..12"
+
+        with self.assertRaises(ValueError) as context:
+            validate_date(data)
+        
+        self.assertEqual(str(context.exception), message)
+    
+    def test_invalid_day(self):
+        data = "1999-12-32"
+        message = "day is out of range for month"
+
+        with self.assertRaises(ValueError) as context:
+            validate_date(data)
+        
+        self.assertEqual(str(context.exception), message)
+    
+    def test_invalid_date(self):
+        data = "invalid-date"
+        message = f"Invalid isoformat string: '{data}'"
+
+        with self.assertRaises(ValueError) as context:
+            validate_date(data)
         
         self.assertEqual(str(context.exception), message)
