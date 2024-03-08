1d053d8d2ef64f4099daddb9a0675322c9f477cd
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-03-08 03:31:42 +0000

    test: Create tests for testing "FlowController" class.

--- tests/test_flow_controller.py
+++ tests/test_flow_controller.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock, call, patch
 
 import phonenumbers
 
-from source.flow_controller import formatted_phone_number, input_handler
+from source.flow_controller import FlowController, formatted_phone_number, input_handler
 
 
 class InputHandler(TestCase):
@@ -45,3 +45,52 @@ class FormattedPhoneNumber(TestCase):
         with self.assertRaises(phonenumbers.phonenumberutil.NumberParseException) as err:
             formatted_phone_number(phone_number)
         self.assertEqual(str(err.exception), "(0) Missing or invalid default region.")
+
+
+class TestFlowController(TestCase):
+
+    @patch.object(FlowController, "manage_options")
+    @patch.object(FlowController, "print_suggestion")
+    def setUp(self, *_):
+        self.sheet = MagicMock()
+        self.flow_controller = FlowController(self.sheet)
+    
+    @patch.object(FlowController, "manage_options")
+    @patch.object(FlowController, "print_suggestion")
+    def test_init(self, mock_print_suggestion, mock_manage_options):
+        mock_sheet = MagicMock()
+        controller_obj = FlowController(mock_sheet)
+
+        self.assertTrue(hasattr(controller_obj, "sheet"))
+        mock_print_suggestion.assert_called_once()
+        mock_manage_options.assert_called_once()
+    
+    @patch.object(FlowController, "create_flow")
+    @patch("source.flow_controller.input_handler")
+    @patch.object(FlowController, "print_suggestion")
+    @patch.object(FlowController, "print_options")
+    def test_menage_options(self,
+                            mock_print_options,
+                            mock_print_suggestion,
+                            mock_input_handler,
+                            mock_create_flow):
+        input_value = "0"
+        mock_input_handler.return_value = input_value
+        mock_create_flow.side_effect = StopIteration
+        with self.assertRaises(StopIteration):
+            self.flow_controller.manage_options()
+        
+        mock_print_options.assert_called_once()
+        mock_print_suggestion.assert_called_once()
+        mock_input_handler.assert_called_once()
+        mock_create_flow.assert_called_once_with(input_value)
+
+    def test_create_flow(self):
+        flow_index = "0"
+
+        with patch.object(FlowController, "FLOW_OPTIONS", new=[{"object": MagicMock()}]) as list_mock:
+            self.flow_controller.create_flow(flow_index)
+        
+        list_mock[int(flow_index)]["object"].assert_called_once_with(self.flow_controller.sheet, self.flow_controller)
+    
+    
